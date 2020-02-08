dbfe2c18ef434ae28ca90dc6feb947cb23c13633
Windows Doni <dony2@hotmail.com>
2020-02-08 16:59:52 +0200

    Firebase analytics added

--- MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
+++ MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.mytestproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         // TODO: Exercise 1 -> Add a new button. Button will show a Toast message "Button clicked!"x1
     }
