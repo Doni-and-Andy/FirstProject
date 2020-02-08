563220c21e7eda0b6410e929e1610db54e793519
Doni Phan <donitsi@Doni-MacBook.local>
2020-02-08 16:25:25 +0200

    Added Test Project

---
+++ MyTestProj/app/src/main/java/com/example/mytestproj/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mytestproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: Exercise 1 -> Add a new button. Button will show a Toast message "Button clicked!"x1
+    }
+}
